a64f295539271b32de3a1229a8bf84a229a57cdf
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-04-24 00:06:34 +0000

    initial website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Jaden Plugowsky All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jaden Plugowsky
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function buttonOneClicked() {
+  //This function calculates the Volume of a Pyramid/Cone
+  //Input through Textfields
+  const baseLength = parseFloat(document.getElementById("base-length").value)
+  const baseWidth = parseFloat(document.getElementById("base-width").value)
+  const height = parseFloat(document.getElementById("height").value)
+
+  //Process
+  const area = baseLength * baseWidth
+  const volume = (area * height) / 3
+
+  //Output
+  document.getElementById("answer-area").innerHTML =
+    "The Area of the Pyramid/Cone's base is: " + area + "cm²"
+  document.getElementById("answer-volume").innerHTML =
+    "The Volume of the Pyramid/Cone is: " + volume.toFixed(2) + "cm³"
+}
